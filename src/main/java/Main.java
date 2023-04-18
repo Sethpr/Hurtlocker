60ae8cb52becdba0b02d4dadb67039a13c0f2aee
Seth <seth@Zipcoders-MacBook-Pro.local>
2023-04-18 13:34:58 -0400

    tightened up some stuff to make it look betterish

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,17 +4,17 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Main {
-    static final String[] name = {"Milk", "Bread","Cookies","Apples"};
+    static final String[] name = {"Apples","Cookies","Bread","Milk"};
 
     public static void main(String[] args) throws Exception{
         String output = IOUtils.toString((new Main()).getClass().getClassLoader().getResourceAsStream("RawData.txt"));
-        StringBuilder sb = new StringBuilder("\nErrors        	 	 seen: " +Pattern.compile("[:@^*%;][:@^*%;]").matcher(output).results().count() +" times");
-        Arrays.stream(name).forEach((n->process(Pattern.compile("(?i)(?<=name:"+n.charAt(0)+".{"+(n.length() - 1)+"};price[:;])[0-9].[0-9][0-9]+(?=;)").matcher(output).results().map((p) -> p.group()).collect(Collectors.toList()), n, sb)));
+        StringBuilder sb = new StringBuilder("\nErrors        	 	seen: "+Pattern.compile("[:@^*%;][:@^*%;]").matcher(output).results().count()+" times");
+        Arrays.stream(name).forEach((n->process(Pattern.compile("(?i)(?<=name:"+n.charAt(0)+".{"+(n.length()-1)+"};price[:;])\\d.\\d\\d(?=;)").matcher(output).results().map((p)->p.group()).collect(Collectors.toList()),n,sb)));
         System.out.println(sb);
     }
 
     public static void process(List<String> list, String name, StringBuilder sb){
-        (new HashSet<>(list)).stream().forEach((p)->sb.insert(0,String.format("Price:   %s       seen: %d times\n-------------       -------------\n",p, Collections.frequency(list, p))));
-        sb.insert(0,"\nname:"+ " ".repeat(8-name.length()) + name+"       seen: " +list.size()+ " times\n=============       =============\n");
+        (new HashSet<>(list)).stream().forEach((p)->sb.insert(0,String.format("Price:   %s       seen: %d times\n-------------       -------------\n",p,Collections.frequency(list,p))));
+        sb.insert(0,"\nname:"+ " ".repeat(8-name.length())+name+"       seen: "+list.size()+ " times\n=============       =============\n");
     }
 }
