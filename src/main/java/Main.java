9fc67df581dc692f8080a88500c9e4b018d64ce5
Seth <seth@Zipcoders-MacBook-Pro.local>
2023-04-18 16:11:42 -0400

    replaced stringbuilder with prints, one less line

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,13 +6,12 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) throws Exception{
         String output = IOUtils.toString((new Main()).getClass().getClassLoader().getResourceAsStream("RawData.txt"));
-        StringBuilder sb = new StringBuilder("\nErrors        	 	seen: "+Pattern.compile("[:@^*%;][:@^*%;]").matcher(output).results().count()+" times");
-        Arrays.stream(new String[]{"Apples","Cookies","Bread","Milk"}).forEach((n->process(Pattern.compile("(?i)(?<=name:"+n.charAt(0)+".{"+(n.length()-1)+"};price[:;])\\d.\\d\\d(?=;)").matcher(output).results().map((p)->p.group()).collect(Collectors.toList()),n,sb)));
-        System.out.println(sb);
+        Arrays.stream(new String[]{"Milk","Bread","Cookies","Apples"}).forEach((n->process(Pattern.compile("(?i)(?<=name:"+n.charAt(0)+".{"+(n.length()-1)+"};price[:;])\\d.\\d\\d(?=;)").matcher(output).results().map((p)->p.group()).collect(Collectors.toList()),n)));
+        System.out.println("\nErrors        	 	seen: "+Pattern.compile("[:@^*%;][:@^*%;]").matcher(output).results().count()+" times");
     }
 
-    public static void process(List<String> list, String name, StringBuilder sb){
-        (new HashSet<>(list)).stream().forEach((p)->sb.insert(0,String.format("Price:   %s       seen: %d times\n-------------       -------------\n",p,Collections.frequency(list,p))));
-        sb.insert(0,"\nname:"+ " ".repeat(8-name.length())+name+"       seen: "+list.size()+ " times\n=============       =============\n");
+    public static void process(List<String> list, String name){
+        System.out.print("\nname:"+ " ".repeat(8-name.length())+name+"       seen: "+list.size()+ " times\n=============       =============\n");
+        (new HashSet<>(list)).stream().forEach((p)->System.out.print(String.format("Price:   %s       seen: %d times\n-------------       -------------\n",p,Collections.frequency(list,p))));
     }
 }
