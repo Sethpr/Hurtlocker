dd9dce0e6dfd80c616d1143a589e1cb893374c64
Seth <seth@Zipcoders-MacBook-Pro.local>
2023-04-18 13:11:10 -0400

    removed a line with smart regex

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,6 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Main {
-    static final String[] pats = {"(?<=name:milk;price[:;])[0-9].[0-9][0-9]+(?=;)", "(?<=name:bread;price[:;])[0-9].[0-9][0-9]+(?=;)", "(?<=name:c[0o][0o]kies;price[:;])[0-9].[0-9][0-9]+(?=;)", "(?<=name:apples;price[:;])[0-9].[0-9][0-9]+(?=;)"};
     static final String[] name = {"Milk", "Bread","Cookies","Apples"};
     public String readRawDataToString() throws Exception{
         ClassLoader classLoader = getClass().getClassLoader();
@@ -17,8 +16,8 @@ public class Main {
     public static void main(String[] args) throws Exception{
         String output = (new Main()).readRawDataToString();
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < pats.length; i++) {
-            sb.append(process(getList(pats[i], output),name[i]));
+        for (int i = 0; i < name.length; i++) {
+            sb.append(process(getList(name[i], output),name[i]));
         }
         sb.append("Errors        	 	 seen: " +Pattern.compile("[:@^*%;][:@^*%;]", Pattern.CASE_INSENSITIVE).matcher(output).results().count() +" times");
         System.out.println(sb);
@@ -32,8 +31,8 @@ public class Main {
 
     //I wanted to put these in line to reduce my line count, but I would like my code to be SOMEWHAT readable. like at least a little
 
-    public static List<String> getList(String pattern, String toMatch){
-        return Pattern.compile(pattern, Pattern.CASE_INSENSITIVE).matcher(toMatch).results().map((p) -> p.group()).collect(Collectors.toList());
+    public static List<String> getList(String name, String toMatch){
+        return Pattern.compile("(?i)(?<=name:"+name.charAt(0)+".{"+(name.length() - 1)+"};price[:;])[0-9].[0-9][0-9]+(?=;)").matcher(toMatch).results().map((p) -> p.group()).collect(Collectors.toList());
     }
 
     public static String pad(String word){
