210239770b5cd01216dc05422c4d80947096dff0
Seth <seth@Zipcoders-MacBook-Pro.local>
2023-04-18 12:36:51 -0400

    cleaned up slightly?

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,14 +4,14 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Main {
-    static final String[] pats = {"(?<=name:milk;price[:;])[0-9].[0-9][0-9]+(?=;)", "(?<=name:bread;price[:;])[0-9].[0-9][0-9]+(?=;)", "(?<=name:c[0o][0o]kies;price[:;])[0-9].[0-9][0-9]+(?=;)", "(?<=name:apples;price[:;])[0-9].[0-9][0-9]+(?=;)"};
+    static final String[] pats = {"(?i)(?<=name:milk;price[:;])[0-9].[0-9][0-9](?=;)", "(?i)(?<=name:bread;price[:;])[0-9].[0-9][0-9](?=;)", "(?i)(?<=name:c[0o][0o]kies;price[:;])[0-9].[0-9][0-9](?=;)", "(?i)(?<=name:apples;price[:;])[0-9].[0-9][0-9](?=;)"};
     static final String[] name = {"Milk", "Bread","Cookies","Apples"};
 
     public static void main(String[] args) throws Exception{
         String output = IOUtils.toString((new Main()).getClass().getClassLoader().getResourceAsStream("RawData.txt"));
         StringBuilder sb = new StringBuilder("\nErrors        	 	 seen: " +Pattern.compile("[:@^*%;][:@^*%;]").matcher(output).results().count() +" times");
         for (int i = pats.length-1; i >= 0; i--) {
-            process(Pattern.compile(pats[i], Pattern.CASE_INSENSITIVE).matcher(output).results().map((p) -> p.group()).collect(Collectors.toList()),name[i], sb);
+            process(Pattern.compile(pats[i]).matcher(output).results().map((p) -> p.group()).collect(Collectors.toList()),name[i], sb);
         }
         System.out.println(sb);
     }
