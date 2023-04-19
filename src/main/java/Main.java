41b478634a69335f3d670aa7c35adcf04eee7627
Seth <seth@DavidszeMacBook.localdomain>
2023-04-19 11:05:18 -0400

    got rid of useless characters, no lines though

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,13 +5,13 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        String output = IOUtils.toString((new Main()).getClass().getClassLoader().getResourceAsStream("RawData.txt"));
-        Arrays.stream(new String[]{"Milk","Bread","Cookies","Apples"}).forEach((n->process(Pattern.compile("(?i)(?<=name:"+n.charAt(0)+".{"+(n.length()-1)+"};price[:;])\\d.\\d\\d(?=;)").matcher(output).results().map((p)->p.group()).collect(Collectors.toList()),n)));
-        System.out.println("\nErrors        	 	seen: "+Pattern.compile("[:@^*%;][:@^*%;]").matcher(output).results().count()+" times");
+        String out = IOUtils.toString((new Main()).getClass().getClassLoader().getResourceAsStream("RawData.txt"));
+        Arrays.stream(new String[]{"Milk","Bread","Cookies","Apples"}).forEach((n->process(Pattern.compile("(?i)(?<=:"+n.charAt(0)+".{3,6};price[:;])\\d.\\d+(?=;)").matcher(out).results().map(p->p.group()).collect(Collectors.toList()),n)));
+        System.out.printf("\nErrors        	 	seen: %d times",Pattern.compile("[:@^*%;][:@^*%;]").matcher(out).results().count());
     }
 
     public static void process(List<String> list, String name){
-        System.out.print("\nname:"+ " ".repeat(8-name.length())+name+"       seen: "+list.size()+ " times\n=============       =============\n");
-        (new HashSet<>(list)).stream().forEach((p)->System.out.print(String.format("Price:   %s       seen: %d times\n-------------       -------------\n",p,Collections.frequency(list,p))));
+        System.out.printf("\nname:%s       seen: %d times\n=============       =============\n"," ".repeat(8-name.length())+name,list.size());
+        (new HashSet<>(list)).stream().forEach(p->System.out.printf("Price:   %s       seen: %d times\n-------------       -------------\n",p,Collections.frequency(list,p)));
     }
 }
